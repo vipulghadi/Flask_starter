fcfb8a30c2a7a2216736d59762fdc4ed0740257d
vipulghadi <vipulvijayghadi@gmail.com>
2025-01-20 21:10:06 +0530

    init

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+from helper import get_video_transcript
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Welcome to the Flask app deployed on AWS Lambda!"})
+
+@app.route('/hello')
+def hello():
+    return jsonify({"message": "Hello, world!"})
+
+@app.route('/get-transcript/<video_id>/')
+def get_transcript(video_id):
+
+    try:
+        transcript = get_video_transcript(f"https://www.youtube.com/watch?v={video_id}")
+        return jsonify({"transcript": transcript})
+    except Exception as e:
+        print(e)
+        return jsonify({
+            "error":True,
+            "message":str(e)
+        })
+    
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ helper.py
@@ -0,0 +1,25 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+
+
+
+def extract_video_id(url):
+    # Check if the URL contains 'youtu.be/' format
+    if 'youtu.be/' in url:
+        video_id_index = url.index('youtu.be/') + len('youtu.be/')
+        video_id = url[video_id_index:].split('?')[0]
+        return video_id
+    # Check if the URL contains 'watch?v=' format
+    elif 'watch?v=' in url:
+        video_id_index = url.index('watch?v=') + len('watch?v=')
+        video_id = url[video_id_index:].split('&')[0]
+        print(video_id)
+        return video_id
+    # If the URL format is not recognized
+    else:
+        return None
+    
+def get_video_transcript(url):
+        video_id=extract_video_id(url)
+        transcript = YouTubeTranscriptApi.get_transcript(video_id,languages=["en","hi"])
+        transcript_text = ' '.join([entry['text'] for entry in transcript])
+        return transcript_text
